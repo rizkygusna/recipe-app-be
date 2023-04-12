24f2bef6b12b94dd5fdfce7cac599056925f89c8
Rizki Alfarid <gusnafarid@gmail.com>
2023-04-12 14:17:59 +0700

    add recipe schema

---
+++ src/models/Recipes.js
@@ -0,0 +1,14 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const RecipeSchema = new mongoose.Schema({
+  name: { type: String, required },
+  ingredients: [{ type: String, required }],
+  instruction: { type: String, required },
+  imgUrl: { type: String, required },
+  cookingTime: { type: Number, required },
+  // refering to user id
+  createdById: { type: mongoose.Schema.Types.ObjectId, ref: "users", required },
+});
+
+// export collection of users to db
+export const UserModel = mongoose.model("recipes", RecipeSchema);
