e96a8cb34e03c389fac0e8b200d861748bf160c9
Rizki Alfarid <gusnafarid@gmail.com>
2023-04-12 15:43:49 +0700

    fix recipes route error

--- src/models/Recipes.js
+++ src/models/Recipes.js
@@ -1,13 +1,17 @@
 import mongoose, { Mongoose } from "mongoose";
 
 const RecipeSchema = new mongoose.Schema({
-  name: { type: String, required },
-  ingredients: [{ type: String, required }],
-  instruction: { type: String, required },
-  imgUrl: { type: String, required },
-  cookingTime: { type: Number, required },
+  name: { type: String, required: true },
+  ingredients: [{ type: String, required: true }],
+  instruction: { type: String, required: true },
+  imgUrl: { type: String, required: true },
+  cookingTime: { type: Number, required: true },
   // refering to user id
-  createdById: { type: mongoose.Schema.Types.ObjectId, ref: "users", required },
+  createdById: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
 });
 
 // export collection of users to db
--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { RecipeModel } from "../models/Recipes";
+import { RecipeModel } from "../models/Recipes.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
